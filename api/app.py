e6e645d044393b00a9101cd64b13985fe04cb0c2
Dmitriy Smirnov <dimkasmir@gmail.com>
2017-02-18 11:56:00 -0800

    Add first histogram trial

--- api/app.py
+++ api/app.py
@@ -1,3 +1,15 @@
 from flask import Flask
+from apiclient.discovery import build
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index_hanlder():
+service = build('customsearch', 'v1', developerKey='AIzaSyBl3oaBTl3QC0hhkJrjKEV9KuGXne0t1Q4')
+request = service.cse().list(q='camel',
+                             searchType='image',
+                             imgDominantColor='pink',
+                             imgSize='medium',
+                             cx='010344681634201659024:28tylyxabkm')
+response = request.execute()
+print len(response['items'])
