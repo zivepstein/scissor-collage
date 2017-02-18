6ec8ff29c849a81bd443cc87ed136efe6b5aa2ce
Dmitriy Smirnov <dimkasmir@gmail.com>
2017-02-18 12:37:30 -0800

    Implement histogram first try

---
+++ api/his.py
@@ -0,0 +1,35 @@
+from scipy.misc import imsave, imread
+import numpy as np
+
+imsrc = imread("source.jpg")
+imtint = imread("target.jpg")
+
+nbr_bins=255
+if len(imsrc.shape) < 3:
+    imsrc = imsrc[:,:,np.newaxis]
+    imtint = imtint[:,:,np.newaxis]
+
+imres = imsrc.copy()
+for d in range(imsrc.shape[2]):
+    imhist,bins = np.histogram(imsrc[:,:,d].flatten(),nbr_bins,normed=True)
+    tinthist,bins = np.histogram(imtint[:,:,d].flatten(),nbr_bins,normed=True)
+
+    cdfsrc = imhist.cumsum() #cumulative distribution function
+    cdfsrc = (255 * cdfsrc / cdfsrc[-1]).astype(np.uint8) #normalize
+
+    cdftint = tinthist.cumsum() #cumulative distribution function
+    cdftint = (255 * cdftint / cdftint[-1]).astype(np.uint8) #normalize
+
+
+    im2 = np.interp(imsrc[:,:,d].flatten(),bins[:-1],cdfsrc)
+
+
+
+    im3 = np.interp(im2,cdftint, bins[:-1])
+
+    imres[:,:,d] = im3.reshape((imsrc.shape[0],imsrc.shape[1] ))
+
+try:
+    imsave("histnormresult.jpg", imres)
+except:
+    imsave("histnormresult.jpg", imres.reshape((imsrc.shape[0],imsrc.shape[1] )))
